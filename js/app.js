07c297d59ccac50b7140eb9a84f47d0bff565921
Cassie <cassiepascual85@gmail.com>
2026-02-09 14:57:02 +0100

    test js

--- js/app.js
+++ js/app.js
@@ -219,12 +219,14 @@ document.addEventListener("DOMContentLoaded", () => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
         entry.target.classList.add("visible");
-      } else {
-        entry.target.classList.remove("visible");
-      }
+        observation.unobserve(entry.target);
+      } //else {
+        //entry.target.classList.remove("visible");
+      //}
     });
   }, {
-    threshold: 0.05 // plus petit = transition plus douce entre les sections
+    threshold: 0.05, // plus petit = transition plus douce entre les sections
+    rootMargin: "0px 0px -100px 0px"
   });
 
   sections.forEach(section => observation.observe(section));
